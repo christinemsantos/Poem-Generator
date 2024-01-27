d875037189acffeb914df5125e4be317a552c12b
Christine Santos <56892403+christinemsantos@users.noreply.github.com>
2024-01-26 20:20:18 -0500

    added submit function and typewriter js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function createPoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem-text", {
+    strings: ["Flowers are pretty"],
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemForm = document.querySelector("#poem");
+poemForm.addEventListener("submit", createPoem);
